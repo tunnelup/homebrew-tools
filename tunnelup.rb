ea69f7259736ff6a6e3881d0f4458afb02df9295
George Drummond <georgedrummond@gmail.com>
2017-05-17 23:59:36 -0400

    Get version one of formula working

--- tunnelup.rb
+++ tunnelup.rb
@@ -1,10 +1,16 @@
 class Tunnelup < Formula
   desc "Tunnelup cli tools"
   homepage "https://tunnelup.com"
+  url "https://github.com/tunnelup/tunnelup-cli.git",
+    revision: "361e8450d748cb729af1495ecab9ced5460d06f0"
+
+  head "https://github.com/tunnelup/tunnelup-cli.git"
 
   depends_on "crystal-lang"
 
   def install
-    puts "Hello World"
+    system "crystal deps"
+    system "make"
+    bin.install "tunnelup"
   end
 end
